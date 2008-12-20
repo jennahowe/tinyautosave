6c36db8f85cbd65c04dee595f0e24b33152c1a84
todd <todd@speednet.biz>
2008-12-20 00:50:06 +0000

    Removed extra comments and tabs from language file, to keep the combined size small.

--- tinyautosave/tinyautosave/langs/en.js
+++ tinyautosave/tinyautosave/langs/en.js
@@ -1,24 +1,5 @@
-/*
-	TinyAutoSave plug-in, by Speednet
-	Adds auto-save capability to the TinyMCE text editor to rescue content inadvertently lost.
-	Copyright © 2008-2009, Speednet Group LLC, All rights reserved.
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-tinyMCE.addI18n('en.tinyautosave', {
-	restore_content: "Restore auto-saved content",
-	no_content: "There is no auto-save content available to restore",
-	warning_message: "If you restore the saved content, you will lose all the content that is currently in the editor.\n\nAre you sure you want to restore the saved content?"
+tinyMCE.addI18n('en.tinyautosave',{
+restore_content: "Restore auto-saved content",
+no_content: "There is no auto-save content available to restore",
+warning_message: "If you restore the saved content, you will lose all the content that is currently in the editor.\n\nAre you sure you want to restore the saved content?"
 });
