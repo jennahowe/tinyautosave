d1e9c211a9673f06d0a89bafa0bea196215729d9
todd <todd@speednet.biz>
2010-05-09 18:14:45 +0000

    Updated to version 2.1.2
    - Changed licensing model from GPL3 to dual-licensed under MIT or GPL2.
    - Updated copyright banners

--- tinyautosave/tinyautosave/editor_plugin_src.js
+++ tinyautosave/tinyautosave/editor_plugin_src.js
@@ -1,28 +1,18 @@
 /*
 	TinyAutoSave plugin for TinyMCE
-	Version: 2.1.1
+	Version: 2.1.2
 	http://tinyautosave.googlecode.com/
 
-	Copyright (c) 2008-2009 Todd Northrop
+	Copyright (c) 2008-2010 Todd Northrop
 	http://www.speednet.biz/
 	
-	December 3, 2009
+	May 9, 2010
 
 	Adds auto-save capability to the TinyMCE text editor to rescue content
 	inadvertently lost.
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	Dual licensed under the MIT or GPL Version 2 licenses.
+	See mit-license.txt and gpl2-license.txt in the project root for details.
 */
 
 
@@ -35,7 +25,7 @@
 	//************************************************************************
 	// PRIVATE VARIABLES
 	
-	var version = "2.1.1",
+	var version = "2.1.2",
 	
 		// The name of the plugin, as specified to TinyMCE
 		pluginName = "tinyautosave",
